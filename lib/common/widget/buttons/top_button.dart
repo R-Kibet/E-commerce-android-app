82f64afa9b14c09996638b273f43a3fbef0574ab
root <richkibz@gmail.com>
2023-03-11 10:14:10 +0300

    creating orders listview

---
+++ lib/common/widget/buttons/top_button.dart
@@ -0,0 +1,33 @@
+import 'package:amazon_clone/common/widget/account_button.dart';
+import 'package:flutter/material.dart';
+
+class TopButtons extends StatefulWidget {
+  const TopButtons({Key? key}) : super(key: key);
+
+  @override
+  State<TopButtons> createState() => _TopButtonsState();
+}
+
+class _TopButtonsState extends State<TopButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            AccountButton(
+                text: "Orders",
+                onTap: () {},
+            ),
+            AccountButton(
+                text: "Wish List",
+                onTap: (){}),
+            AccountButton(
+                text: "turn seller",
+                onTap: () {})
+          ],
+        ),
+      ],
+    );
+  }
+}
