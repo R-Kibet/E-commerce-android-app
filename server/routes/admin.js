64e29cbb1b70324b0263dcdf3e4a2c5d74d6f336
root <richkibz@gmail.com>
2023-03-19 18:58:14 +0300

    producta and admin section

---
+++ server/routes/admin.js
@@ -0,0 +1,22 @@
+const express  = require('express')
+const adminRouter = express.Router();
+const admin = require("../middlewares/admin")
+
+//Add product
+adminRouter.post("/admin/add-product", admin, async (req, res) => {
+    try {
+        const {name, description, image, quantity, price, category} = req.body;
+        let product = new Product ({
+            name,
+            description,
+            images,
+            quantity,
+            price,
+            category,
+        });
+        product =  await product.save();
+        res.json(product);
+    } catch (e) {
+        es.status(500).json({error. e.message });
+    }
+})
